9c66fdaab093b5cf46870af39bee3c625f1a84cc
nawafmir <14013@kiet.edu.pk>
2025-07-07 22:40:06 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Generative Ai Projects',
+    version='0.0.0',
+    author='Nawaf mir',
+    author_email='Nawafmeer17@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.py
@@ -0,0 +1,31 @@
+import os 
+from pathlib import Path
+import logging 
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    fieldir,filename=os.path.split(filepath)
+
+    if fieldir !="":
+        os.makedirs(fieldir, exist_ok=True)
+        logging.info(f"Created directory: {fieldir} for the file : {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f"Created file: {filepath}")
+    else:
+        logging.info(f"File already exists and is not empty: {filepath}")
